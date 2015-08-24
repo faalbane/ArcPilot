1b08545778d829c78eca4df3190c263a0fd4a3f4
adlazare <adlazare@oakland.edu>
2015-08-24 19:23:41 -0400

    Making Progress
    2D pong coming along nicely

--- ArcPilot-UnityFiles/Assets/PlayerControls.js
+++ ArcPilot-UnityFiles/Assets/PlayerControls.js
@@ -2,8 +2,6 @@
 
 var moveUp : KeyCode;
 var moveDown : KeyCode;
-var moveLeft : KeyCode;
-var moveRight : KeyCode;
 
 var speed : float = 10;
 
@@ -17,16 +15,9 @@ function Update ()
 	{
 		GetComponent.<Rigidbody2D>().velocity.y = speed *-1;
 	}
-	else if (Input.GetKey(moveLeft))
-	{
-		GetComponent.<Rigidbody2D>().velocity.x = speed *-1;
-	}
-	else if (Input.GetKey(moveRight))
-	{
-		GetComponent.<Rigidbody2D>().velocity.x = speed;
-	}
 	else
 	{
 		GetComponent.<Rigidbody2D>().velocity.y = 0;
+		GetComponent.<Rigidbody2D>().velocity.x = 0;
 	}
 }
