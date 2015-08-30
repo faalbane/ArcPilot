f3dca60453ea9d1c1db3c87d9107ab361e8a3369
adlazare <adlazare@oakland.edu>
2015-08-29 21:25:56 -0400

    Pong Tutorial Completed!
    Check it out! Run either the standalone version or the web version to
    play it.

--- ArcPilot-UnityFiles/Assets/GameManager.js
+++ ArcPilot-UnityFiles/Assets/GameManager.js
@@ -5,6 +5,12 @@ static var playerScore02 : int = 0;
 
 var theSkin : GUISkin;
 
+var theBall : Transform;
+
+function Start() {
+	theBall = GameObject.FindGameObjectWithTag ("Ball").transform; 
+}
+
 static function Score (wallName : String) {
 	if (wallName == "rightWall") 
 	{
@@ -20,6 +26,13 @@ static function Score (wallName : String) {
 
 function OnGUI () {
 	GUI.skin = theSkin;
-	GUI.Label (new Rect (Screen.width/2-150, 25, 100, 100), "" + playerScore01);
-	GUI.Label (new Rect (Screen.width/2+150, 25, 100, 100), "" + playerScore02);
+	GUI.Label (new Rect (Screen.width/2-150-18, 25, 100, 100), "" + playerScore01);
+	GUI.Label (new Rect (Screen.width/2+150-18, 25, 100, 100), "" + playerScore02);
+	
+	if (GUI.Button (new Rect(Screen.width/2-121/2, 35, 121, 53), "RESET")){
+		playerScore01 = 0;
+		playerScore02 = 0;
+		
+		theBall.gameObject.SendMessage ("ResetBall");
 	}
+}
--- ArcPilot-UnityFiles/Assets/SideWalls.js
+++ ArcPilot-UnityFiles/Assets/SideWalls.js
@@ -4,6 +4,8 @@ function OnTriggerEnter2D (hitInfo : Collider2D) {
 	if (hitInfo.name == "Ball")
 	{
 		var wallName = transform.name;
+		GetComponent.<AudioSource>().Play();
 		GameManager.Score (wallName);
+		hitInfo.gameObject.SendMessage ("ResetBall");
 	}
 }
